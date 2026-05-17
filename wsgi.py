767584f9c592d0e7b94974c9d48b7f4eade41a68
EdgarLedoux <ledoux.edgar@gmail.com>
2026-05-17 14:38:00 +0200

    wsgi - venvpath

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 
 # Adjust this path to your actual virtualenv and project location on AlwaysData
 # Replace <your_username> with your AlwaysData account name
-VENV_PATH = "/home/gomoku/.local/share/venvs/default/lib/python3.11/site-packages"
+VENV_PATH = "/home/gomoku/.local/lib/python3.13/site-packages"
 PROJECT_PATH = "/home/gomoku/gomoku"
 if VENV_PATH not in sys.path:
     sys.path.insert(0, VENV_PATH)
